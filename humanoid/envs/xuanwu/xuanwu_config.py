e43f252442004c6aed9a0d49f10b93ec001a2a17
Yuxi Liu <yuxiliu0403@email.com>
2025-03-27 08:46:57 -0400

    feat: new trained policy v8, added disturbance in Sim2Sim to verify trained policy

--- humanoid/envs/xuanwu/xuanwu_config.py
+++ humanoid/envs/xuanwu/xuanwu_config.py
@@ -155,7 +155,7 @@ class XuanwuCfg(LeggedRobotCfg):
         added_mass_range = [-1.0, 1.0]
         push_robots = True
         push_interval_s = 4
-        max_push_vel_xy = 0.2
+        max_push_vel_xy = 0.4
         max_push_ang_vel = 0.4
         # dynamic randomization
         action_delay = 0.5
@@ -175,25 +175,25 @@ class XuanwuCfg(LeggedRobotCfg):
 
     class rewards:
         base_height_target = 0.55
-        min_dist = 0.16
-        max_dist = 0.30
+        min_dist = 0.12
+        max_dist = 0.24
         # put some settings here for LLM parameter tuning
         target_joint_pos_scale = 0.17    # rad
         target_feet_height = 0.05        # m
-        cycle_time = 1.0                # sec
+        cycle_time = 0.64             # sec
         # if true negative total rewards are clipped at zero (avoids early termination problems)
         only_positive_rewards = True
         # tracking reward = exp(error*sigma)
         tracking_sigma = 5
-        max_contact_force = 80  # Forces above this value are penalized
+        max_contact_force = 100  # Forces above this value are penalized
 
         class scales:
             # reference motion tracking
             joint_pos = 1.6
-            feet_clearance = 1.2
+            feet_clearance = 1.
             feet_contact_number = 1.2
             # gait
-            feet_air_time = 1.2
+            feet_air_time = 1.
             foot_slip = -0.05
             feet_distance = 0.2
             knee_distance = 0.2
